b6f6457b71604a26a0e645d3e0e65a46fd8b5e90
Lakshan Ruwinda <112312002+shan-ganegoda@users.noreply.github.com>
2023-03-29 11:39:49 +0530

    initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,58 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HomeComponent} from './view/home/home.component';
+import {LoginComponent} from './view/login/login.component';
+import {MainwindowComponent} from './view/mainwindow/mainwindow.component';
+import {EmployeeComponent} from './view/modules/employee/employee.component';
+import {UserComponent} from './view/modules/user/user.component';
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatIconModule} from "@angular/material/icon";
+import {MessageComponent} from "./util/dialog/message/message.component";
+import {MatDialogModule} from "@angular/material/dialog";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    MainwindowComponent,
+    EmployeeComponent,
+    UserComponent,
+    MessageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatDialogModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
